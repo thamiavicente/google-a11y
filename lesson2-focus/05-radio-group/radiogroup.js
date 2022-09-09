8b44b674430a8e58bc0ae08fe6dc36d4295bda28
thamiris.vicente <thamiris.vicente@valtech.com>
2022-09-09 19:58:05 -0300

    answer: lesson 02 - exercise 05

--- lesson2-focus/05-radio-group/radiogroup.js
+++ lesson2-focus/05-radio-group/radiogroup.js
@@ -31,7 +31,9 @@
 
         e.preventDefault();
 
-        // This seems like a good place to do some stuff :)
+        if(this.focusedIdx === 0) this.focusedIdx = this.buttons.length -1;
+        else this.focusedIdx --;
+
 
         break;
 
@@ -42,7 +44,8 @@
 
         e.preventDefault();
 
-        // This seems like a good place to do some stuff :)
+        if(this.focusedIdx >= this.buttons.length -1) this.focusedIdx = 0;
+        else this.focusedIdx ++;
 
         break;
       }
